09ad8621f14cf434562909d14092d3aadd090115
heardMan <markyheard@gmail.com>
2018-12-15 15:10:33 -0800

    hooked up create post form

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,36 @@ $("#menu-toggle").click(function (e) {
 $("#pull").click(function () {
     $('#form').toggleClass('hide');
 });
+
+function writeNewPost(name, date, contact) {
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+
+    var postData = {
+        postKey: newPostKey,
+        userName: name,
+        departureDate: date,
+        contactInfo: contact,
+    };
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+    var updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    return firebase.database().ref().update(updates);
+}
+
+function createNewPost() {
+    $("#submitNewPost").on("click", function (e) {
+        e.preventDefault();
+        var postName = $("#newPostName").val();
+        var postDate = $("#newPostDate").val();
+        //var postOrigin = $("#origin").val();
+        var postContact = $("#newPostPhone").val();
+        writeNewPost(postName, postDate, postContact);
+        $("#newPostName").val("");
+        $("#newPostDate").val("");
+        //$("#origin").val("");
+        $("#newPostPhone").val("");
+
+    })
+
+}
+createNewPost();
